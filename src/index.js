21e6f352b7bdc6ea51caf3ccfc1eb6c9ec71bf34
Mario Villacreses <MarioVillacreses@outlook.com>
2018-09-22 12:44:59 -0400

    Add React Router to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import history from './history';
+import {Router} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={history}> 
+    <App />
+  </Router>, 
+  document.getElementById('root'));
 registerServiceWorker();
